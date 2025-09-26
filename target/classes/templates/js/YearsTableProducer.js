f24d91da7508dfaee76dabe6813daf0c526b1884
Kukuruzvelt1024 <boss-96@yandex.ru>
2025-09-26 20:11:37 +0300

    co

---
+++ target/classes/templates/js/YearsTableProducer.js
@@ -0,0 +1,21 @@
+let yearstable = document.createElement('table');
+        yearstable.setAttribute("align", "center");
+        yearstable.setAttribute("class", "table_of_years");
+        fetch("/yearsjson").then(response => {
+            if (!response.ok) {throw new Error(`HTTP error! status: ${response.status}`);}
+            return response.json(); // Преобразование ответа в JSON
+       })
+       .then(data => {
+            var row = yearstable.insertRow();
+            let allYearCellReference = row.insertCell();
+            allYearCellReference.innerHTML = "<p><a href=?year=all>Все</a><p/>"
+             for (const entity of data){
+                let cell = row.insertCell();
+                cell.innerHTML =
+                "<p><a href=?year=" + entity + ">" + entity + "</a></p>"
+            }
+       })
+       .catch(error => {
+             console.error('Ошибка при получении данных:', error);
+       });
+     document.getElementById("Filter_by_year").appendChild(yearstable);
